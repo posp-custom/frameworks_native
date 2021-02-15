SOKP/android_frameworks_native

input: Adjust priority
Change-Id: Ic0477225e9b0bb3eb4f2c71ab3b2620f56d947c7
Signed-off-by: Hriday Sharma <hridaysharma42@gmail.com>

services/surfaceflinger/DispSync.cpp
@@ -22,6 +22,7 @@
 
 #include <math.h>
 
+#include <cutils/iosched_policy.h>
 #include <cutils/log.h>
 
 #include <ui/Fence.h>
@@ -383,13 +384,14 @@ DispSync::DispSync(const char* name) :
         mThread(new DispSyncThread(name)) {
 
     mThread->run("DispSync", PRIORITY_URGENT_DISPLAY + PRIORITY_MORE_FAVORABLE);
+    android_set_rt_ioprio(mThread->getTid(), 1);
+
     // set DispSync to SCHED_FIFO to minimize jitter
     struct sched_param param = {0};
     param.sched_priority = 2;
     if (sched_setscheduler(mThread->getTid(), SCHED_FIFO, &param) != 0) {
         ALOGE("Couldn't set SCHED_FIFO for DispSyncThread");
     }
-
 
     reset();
     beginResync();

posp-custom/frameworks_native

services/surfaceflinger/Scheduler/DispSync.cpp
@@ -29,6 +29,7 @@
 #include <algorithm>
 
 #include <android-base/stringprintf.h>
+#include <bfqio/bfqio.h>
 #include <cutils/properties.h>
 #include <log/log.h>
 #include <utils/Thread.h>
@@ -503,6 +504,8 @@ DispSync::DispSync(const char* name, bool hasSyncFramework)
     if (sched_setscheduler(mThread->getTid(), SCHED_FIFO, &param) != 0) {
         ALOGE("Couldn't set SCHED_FIFO for DispSyncThread");
     }
+
+    android_set_rt_ioprio(mThread->getTid(), 1);
 
     beginResync();
 